bde824c60731b74ea1d9e87156f556aed3d9e893
import-username <stermanv2@gmail.com>
2021-10-18 19:44:53 -0400

    Added IHttpRequestAction functional interface

---
+++ app/src/main/java/com/importusername/musicplayer/interfaces/IHttpRequestAction.java
@@ -0,0 +1,11 @@
+package com.importusername.musicplayer.interfaces;
+
+/**
+ * Functional interface used to perform action after request to music player server has been made.
+ */
+public interface IHttpRequestAction {
+    /**
+     * @param status Response status code from http request.
+     */
+    void requestAction(int status);
+}
