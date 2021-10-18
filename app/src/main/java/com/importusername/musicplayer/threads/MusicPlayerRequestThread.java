9e7140122daafeab66907142f788e7e8a41b56b5
import-username <stermanv2@gmail.com>
2021-10-18 19:42:40 -0400

    Added MusicPlayerRequestThread class

---
+++ app/src/main/java/com/importusername/musicplayer/threads/MusicPlayerRequestThread.java
@@ -0,0 +1,37 @@
+package com.importusername.musicplayer.threads;
+
+import com.importusername.musicplayer.http.MusicPlayerRequest;
+import com.importusername.musicplayer.interfaces.IHttpRequestAction;
+
+import java.io.IOException;
+
+/**
+ * Thread for sending http request to music player server.
+ */
+public class MusicPlayerRequestThread extends Thread {
+    private final String url;
+
+    private final IHttpRequestAction requestAction;
+
+    /**
+     * @param url Url to send request to.
+     * @param action 
+     */
+    public MusicPlayerRequestThread(String url, IHttpRequestAction action) {
+        this.url = url;
+        this.requestAction = action;
+    }
+
+    @Override
+    public void run() {
+        final MusicPlayerRequest musicPlayerRequest = new MusicPlayerRequest(this.url);
+
+        try {
+            musicPlayerRequest.get();
+
+            this.requestAction.requestAction(musicPlayerRequest.getStatus());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
