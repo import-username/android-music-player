09570d17280374ba4b30732e4a5176884541dcda
import-username <stermanv2@gmail.com>
2021-10-19 18:32:13 -0400

    Music player navigation view now changes music player activity fragment corresponding to the item that was selected

--- app/src/main/java/com/importusername/musicplayer/activity/MusicPlayerActivity.java
+++ app/src/main/java/com/importusername/musicplayer/activity/MusicPlayerActivity.java
@@ -6,6 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.importusername.musicplayer.R;
+import com.importusername.musicplayer.fragments.HomeMenuFragment;
+import com.importusername.musicplayer.fragments.PlaylistsMenuFragment;
+import com.importusername.musicplayer.fragments.SettingsMenuFragment;
+import com.importusername.musicplayer.fragments.SongsMenuFragment;
 
 public class MusicPlayerActivity extends AppCompatActivity {
     private final FragmentManager fragmentManager = this.getSupportFragmentManager();
@@ -18,12 +22,36 @@ public class MusicPlayerActivity extends AppCompatActivity {
 
         BottomNavigationView navigationView = findViewById(R.id.music_player_navbar);
 
-//        navigationView.setOnItemSelectedListener((item) -> {
-//            switch(item.getTitle().toString()) {
-//                case "Home":
-//                    System.out.println("abc");
-//                    break;
-//            }
-//        });
+
+        navigationView.setOnItemSelectedListener((item) -> {
+            switch(item.getTitle().toString()) {
+                case "Home":
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.music_player_fragment_view, HomeMenuFragment.class, null)
+                            .setReorderingAllowed(true)
+                            .commit();
+                    break;
+                case "Songs":
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.music_player_fragment_view, SongsMenuFragment.class, null)
+                            .setReorderingAllowed(true)
+                            .commit();
+                    break;
+                case "Playlists":
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.music_player_fragment_view, PlaylistsMenuFragment.class, null)
+                            .setReorderingAllowed(true)
+                            .commit();
+                    break;
+                case "Settings":
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.music_player_fragment_view, SettingsMenuFragment.class, null)
+                            .setReorderingAllowed(true)
+                            .commit();
+                    break;
+            }
+
+            return true;
+        });
     }
 }
