8cf5dda4107b602593648939af818577bc336b16
import-username <stermanv2@gmail.com>
2021-10-19 09:35:41 -0400

    Added music player activity

---
+++ app/src/main/java/com/importusername/musicplayer/activity/MusicPlayerActivity.java
@@ -0,0 +1,14 @@
+package com.importusername.musicplayer.activity;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MusicPlayerActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
