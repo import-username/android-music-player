cea725dd8cff2e95ffd5049173d3d2f541fd8f68
import-username <stermanv2@gmail.com>
2021-10-19 17:34:43 -0400

    Added bottom navigation view to music player activity

--- app/src/main/java/com/importusername/musicplayer/activity/MusicPlayerActivity.java
+++ app/src/main/java/com/importusername/musicplayer/activity/MusicPlayerActivity.java
@@ -3,12 +3,24 @@ package com.importusername.musicplayer.activity;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.importusername.musicplayer.R;
 
 public class MusicPlayerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_music_player_menu);
 
+        BottomNavigationView navigationView = findViewById(R.id.music_player_navbar);
+
+//        navigationView.setOnItemSelectedListener((item) -> {
+//            switch(item.getTitle().toString()) {
+//                case "Home":
+//                    System.out.println("abc");
+//                    break;
+//            }
+//        });
     }
 }
