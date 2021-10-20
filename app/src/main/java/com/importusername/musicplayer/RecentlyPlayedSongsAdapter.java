b3b923e0655715f9041236ea8059d9d211df647e
import-username <stermanv2@gmail.com>
2021-10-20 10:55:17 -0400

    Added recyclerview adapter class for recently played songs recyclerview in home menu fragment layout

---
+++ app/src/main/java/com/importusername/musicplayer/RecentlyPlayedSongsAdapter.java
@@ -0,0 +1,69 @@
+package com.importusername.musicplayer;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class RecentlyPlayedSongsAdapter extends RecyclerView.Adapter<RecentlyPlayedSongsAdapter.ViewHolder> {
+    private Set<String> recentlyPlayedSongs;
+
+    /**
+     * Initialize recentlyPlayedSongs set
+     * @param recentlyPlayedSongsSet Set containing recently played songs.
+     */
+    public RecentlyPlayedSongsAdapter(Set<String> recentlyPlayedSongsSet) {
+        this.recentlyPlayedSongs = recentlyPlayedSongsSet;
+    }
+
+    @NotNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.recently_played_song_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ViewHolder holder, int position) {
+        final ConstraintLayout recentlyPlayedSongsContainer = holder.getRecentlyPlayedSongContainer();
+        final int childrenCount = holder.getRecentlyPlayedSongContainer().getChildCount();
+
+        for (int i = 0; i < childrenCount; i++) {
+            final View child = recentlyPlayedSongsContainer.getChildAt(i);
+            if (child instanceof TextView) {
+                final TextView childTextView = ((TextView) child);
+                final String text = childTextView.getText() + "|" + new ArrayList<>(this.recentlyPlayedSongs).get(i);
+                childTextView.setText(text);
+            }
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.recentlyPlayedSongs.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final ConstraintLayout recentlyPlayedSongContainer;
+
+        public ViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+
+            recentlyPlayedSongContainer = itemView.findViewById(R.id.recently_played_song_container);
+        }
+
+        public ConstraintLayout getRecentlyPlayedSongContainer() {
+            return this.recentlyPlayedSongContainer;
+        }
+    }
+}
--- app/src/main/java/com/importusername/musicplayer/fragments/HomeMenuFragment.java
+++ app/src/main/java/com/importusername/musicplayer/fragments/HomeMenuFragment.java
@@ -1,10 +1,46 @@
 package com.importusername.musicplayer.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.importusername.musicplayer.R;
+import com.importusername.musicplayer.RecentlyPlayedSongsAdapter;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
 
 public class HomeMenuFragment extends Fragment {
     public HomeMenuFragment() {
         super(R.layout.music_player_home_menu_fragment);
+    }
+
+    @Override
+    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater,
+                             @Nullable @org.jetbrains.annotations.Nullable ViewGroup container,
+                             @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.music_player_home_menu_fragment, container, false);
+
+        final HashSet<String> recentlyPlayedSongs = new HashSet<>();
+        // TODO - add event listener that displays recently played songs recyclerview when song is added to recently played list
+
+        final RecyclerView recyclerView = rootView.findViewById(R.id.recently_played_songs_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
+        RecentlyPlayedSongsAdapter recentlyPlayedSongsAdapter = new RecentlyPlayedSongsAdapter(recentlyPlayedSongs);
+        recyclerView.setAdapter(recentlyPlayedSongsAdapter);
+
+        return rootView;
     }
 }
