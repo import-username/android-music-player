95e8ff9f88f89546ae9a9a799b4b5aceeceea4e8
import-username <stermanv2@gmail.com>
2021-11-09 20:33:08 -0500

    Added fragment which allows user to change song attributes such as title, author, etc before being sent to server

---
+++ app/src/main/java/com/importusername/musicplayer/fragments/CreateSongItemMenuFragment.java
@@ -0,0 +1,10 @@
+package com.importusername.musicplayer.fragments;
+
+import androidx.fragment.app.Fragment;
+import com.importusername.musicplayer.R;
+
+public class CreateSongItemMenuFragment extends Fragment {
+    public CreateSongItemMenuFragment() {
+        super(R.layout.create_song_item_menu_fragment);
+    }
+}
