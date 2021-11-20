e266ff2eea230020ce81de701e268c0afe0bf4a4
import-username <stermanv2@gmail.com>
2021-11-20 08:25:45 -0500

    Added class of constant endpoints for sending requests to server

---
+++ app/src/main/java/com/importusername/musicplayer/constants/Endpoints.java
@@ -0,0 +1,8 @@
+package com.importusername.musicplayer.constants;
+
+/**
+ * Class of constant strings representing server endpoints.
+ */
+public class Endpoints {
+    public static final String GET_SONGS = "/song/get-songs";
+}
