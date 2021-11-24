e732c501ae1816c8696d8965b08e817a8a8475c1
import-username <stermanv2@gmail.com>
2021-11-24 16:16:05 -0500

    Added functional interface for setting click listener on song items

---
+++ app/src/main/java/com/importusername/musicplayer/interfaces/ISongItemListener.java
@@ -0,0 +1,7 @@
+package com.importusername.musicplayer.interfaces;
+
+import com.importusername.musicplayer.adapters.songsmenu.SongsMenuItem;
+
+public interface ISongItemListener {
+    void songItemClickListener(SongsMenuItem songsMenuItem);
+}
