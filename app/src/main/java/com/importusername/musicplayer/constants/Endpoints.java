0d617967b5da7cd8050853a6bdb9cbda2fbeb59b
import-username <stermanv2@gmail.com>
2021-12-01 17:08:58 -0500

    Added some more endpoint constants

--- app/src/main/java/com/importusername/musicplayer/constants/Endpoints.java
+++ app/src/main/java/com/importusername/musicplayer/constants/Endpoints.java
@@ -6,5 +6,9 @@ package com.importusername.musicplayer.constants;
 public class Endpoints {
     public static final String GET_SONGS = "/song/get-songs";
 
+    public static final String GET_SONG = "/song/get-song";
+
     public static final String GET_THUMBNAIL = "/song/get-thumbnail";
+
+    public static final String SONG = "/song";
 }
